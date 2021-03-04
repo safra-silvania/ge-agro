5b6cbb2fac88874ddaf7336b86ab8adcaf83a989
Gustavo César <gustavo.cesar123@outlook.com>
2021-03-04 08:39:16 -0300

    Editor

--- templates/Pages/edit.php
+++ templates/Pages/edit.php
@@ -1,6 +1,6 @@
-<?php $this->Form->setTemplates(['inputContainer' => '<div class="col-lg-5 {{required}}">{{content}} </div>','inputContainerError' => '<div class="col-md-5 alert alert-warning {{required}}">{{content}} <strong>{{error}}</strong> </div>']);?>
+<?php $this->Form->setTemplates(['inputContainer' => '<div class="col-lg-5 {{required}}">{{content}} </div>', 'inputContainerError' => '<div class="col-md-5 alert alert-warning {{required}}">{{content}} <strong>{{error}}</strong> </div>']);?>
 
-<?= $this->Form->create($page, ['class' => 'form-horizontal']) ?>
+<?=$this->Form->create($page, ['class' => 'form-horizontal'])?>
 
 <div class="row">
     <div class="col-md-12">
@@ -19,28 +19,28 @@
                 <div class="padd">
 
                     <div class="form-group">
-                        <label class="col-lg-2 control-label required-label" for="name"><?= __('Name') ?></label>
-                        <?= $this->Form->control('name', ['class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label required-label" for="name"><?=__('Name')?></label>
+                        <?=$this->Form->control('name', ['class' => 'form-control', 'label' => false])?>
                     </div>
                     <div class="form-group">
-                        <label class="col-lg-2 control-label required-label" for="action"><?= __('Action') ?></label>
-                        <?= $this->Form->control('action', ['class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label required-label" for="action"><?=__('Action')?></label>
+                        <?=$this->Form->control('action', ['class' => 'form-control', 'label' => false])?>
                     </div>
                     <div class="form-group">
-                        <label class="col-lg-2 control-label required-label" for="page_status_id"><?= __('Page_status_id') ?></label>
-                        <?= $this->Form->control('page_status_id', ['options' => $pageStatuses, 'class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label required-label" for="page_status_id"><?=__('Page_status_id')?></label>
+                        <?=$this->Form->control('page_status_id', ['options' => $pageStatuses, 'class' => 'form-control', 'label' => false])?>
                     </div>
                     <div class="form-group">
-                        <label class="col-lg-2 control-label required-label" for="tenant_id"><?= __('Tenant_id') ?></label>
-                        <?= $this->Form->control('tenant_id', ['options' => $tenants, 'class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label required-label" for="tenant_id"><?=__('Tenant_id')?></label>
+                        <?=$this->Form->control('tenant_id', ['options' => $tenants, 'class' => 'form-control', 'label' => false])?>
                     </div>
                     <div class="form-group">
-                        <label class="col-lg-2 control-label required-label" for="order"><?= __('Order') ?></label>
-                        <?= $this->Form->control('order', ['class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label required-label" for="order"><?=__('Order')?></label>
+                        <?=$this->Form->control('order', ['class' => 'form-control', 'label' => false])?>
                     </div>
                     <div class="form-group">
-                        <label class="col-lg-2 control-label" for="content"><?= __('Content') ?></label>
-                        <?= $this->Form->control('content', ['class' => 'form-control', 'label' => false]) ?>
+                        <label class="col-lg-2 control-label" for="content"><?=__('Content')?></label>
+                        <?=$this->Form->control('content', ['class' => 'form-control ck-editor', 'label' => false])?>
                     </div>
                 </div>
             </div>
@@ -49,16 +49,16 @@
                 <?php echo $this->Form->button('Gravar', ['class' => 'btn btn-primary']); ?>
 
                 <?php
-                echo $this->Html->link(
-                    '<button type="button" class="btn btn-default">Cancelar</button>',
-                    ['action' => 'index'],
-                    ['escape' => false, 'title' => 'Cancelar']
-                );
-                ?>
+echo $this->Html->link(
+    '<button type="button" class="btn btn-default">Cancelar</button>',
+    ['action' => 'index'],
+    ['escape' => false, 'title' => 'Cancelar']
+);
+?>
             </div>
 
         </div>
     </div>
 </div>
 
-<?= $this->Form->end() ?>
+<?=$this->Form->end()?>
--- templates/Site/cursos.php
+++ templates/Site/cursos.php
@@ -1,10 +1,8 @@
 <?=$this->element('breadcrumb')?>
 
-<section class="content contact">
-    <article class="main">
-        <h1><?=$page->name?></h1>
-        <?=$page->content?>
-    </article>
+<section class="content">
+    <h1><?=$page->name?></h1>
+    <?=$page->content?>
 </section>
 
 <?php
